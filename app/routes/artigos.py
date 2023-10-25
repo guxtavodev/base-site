4a700985f5890fa3ddb8ff54a2dd726197f02cbc
guxtavodev <88405978+guxtavodev@users.noreply.github.com>
2023-10-25 00:36:05 +0000

    estrutura inicial padrao

---
+++ app/routes/artigos.py
@@ -0,0 +1,5 @@
+# from flask import render_template, redirect, session, jsonify, request, url_for, make_response
+# from app.routes import artigos_bp
+# from app.models import Artigo, User
+# from app import db
+# from passlib.hash import bcrypt_sha256
